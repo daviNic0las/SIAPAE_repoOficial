4a5fbadaaee3609dd6909e67ead0ff641e77a363
daviNic0las <davi.oliveira102@aluno.ce.gov.br>
2024-12-16 08:33:15 -0300

    Calendário e tradução feitos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href=" {{ asset('css/etc.css') }} ">
     
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/themes/material_green.css">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
